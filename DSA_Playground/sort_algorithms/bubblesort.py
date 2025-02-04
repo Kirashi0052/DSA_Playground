bf5a18f7bb9893ed6081e791785bbb9b26d7761f
Kirashi0052 <alex-ras@web.de>
2025-02-04 15:23:48 +0100

    bubble sort usw.

---
+++ DSA_Playground/sort_algorithms/bubblesort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        for n in range(0, len(arr) - 1):
+            if arr[n] >= arr[n + 1]:
+                temp = arr[n]
+                arr[n] = arr[n + 1]
+                arr[n + 1] = temp
+    return arr
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = bubble_sort(numbers)
+print(sorted_numbers)  # Expected Output [11, 12, 22, 25, 34, 64, 90]
