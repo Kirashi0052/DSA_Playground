698c2764f88dce97d43817f909e8d8bf01f473b1
Kirashi0052 <alex-ras@web.de>
2025-02-06 09:55:26 +0100

    test commmit

---
+++ DSA_Playground/sort_algorithms/selectionsort/selectionsort.py
@@ -0,0 +1,13 @@
+def selection_sort(arr):
+    for i in range(0, len(arr) - 1):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+test_array = [64, 34, 25, 12, 22, 11, 90]
+
+print(selection_sort(test_array))
