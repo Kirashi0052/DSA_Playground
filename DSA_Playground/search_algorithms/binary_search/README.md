c646e3e0920f00ded5695a5c680791f3a5b7af13
Kirashi0052 <alex-ras@web.de>
2025-02-07 14:44:23 +0100

    binarysearch

---
+++ DSA_Playground/search_algorithms/binary_search/README.md
@@ -0,0 +1,11 @@
+# Binary search algorithm
+
+The Binary search algorithm is a basic algortihm that searches an Element in an sorted array.
+
+## Complexity
+
+## Visualization
+
+## Intended use
+
+## Code implementation
--- DSA_Playground/search_algorithms/binary_search/binarysearch.py
+++ DSA_Playground/search_algorithms/binary_search/binarysearch.py
@@ -1,5 +1,17 @@
-def binary_search(arr):
-    pass
-
+def binary_search(arr, search_value):
+    minimum = 0
+    maximum = len(arr) - 1
+    while minimum <= maximum:
+        mid = (maximum + minimum) // 2
+        if arr[mid] == search_value:
+            return True
+        else:
+            if search_value < arr[mid]:
+                maximum = mid - 1
+            else:
+                minimum = mid + 1
+    return False
+            
 
 sorted_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(binary_search(sorted_array, 7))
